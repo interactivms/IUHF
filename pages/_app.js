4082c7b53b55e8fbd8626c5ad4533fef53df6e14
AlexisDguezR <89795338+AlexisDguezR@users.noreply.github.com>
2022-07-27 15:38:52 -0500

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="shortcut icon" href="public/favicon.ico" />
+        <link rel="shortcut icon" href="favicon.ico" />
         <title>Instituo Universitario de las Habilidades del Futuro</title>
         <meta
           name="description"
